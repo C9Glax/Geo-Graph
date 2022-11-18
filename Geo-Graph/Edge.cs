4e5507f68944a10b085a2ca2a9f8eb9299b20eb6
C9Glax <13404778+C9Glax@users.noreply.github.com>
2022-11-19 00:29:55 +0100

    Moved Way here https://github.com/C9Glax/OSM-XML-Importer/commit/1e62f83f975c69355d41f1e704e5ebb7a4072f62

--- Geo-Graph/Edge.cs
+++ Geo-Graph/Edge.cs
@@ -4,20 +4,20 @@
     {
         public ulong id { get; }
         public Node neighbor { get; }
-        public byte maxspeed { get; }
+        public Way way { get; }
         public float distance { get; }
 
-        public Edge(Node neighbor, byte maxspeed, float distance, ulong? id)
+        public Edge(Node neighbor, Way way, float distance, ulong? id)
         {
             this.neighbor = neighbor;
-            this.maxspeed = maxspeed;
+            this.way = way;
             this.distance = distance;
             this.id = (id != null) ? id.Value : 0;
         }
 
         public override string ToString()
         {
-            return string.Format("Edge ID: {0} Maxspeed: {1} Distance: {2:0000.00}m", this.id, this.maxspeed, this.distance);
+            return string.Format("Edge ID: {0} Maxspeed: {1} Distance: {2:0000.00}m", this.id, this.way.GetMaxSpeed(Way.speedType.car), this.distance);
         }
     }
 }
