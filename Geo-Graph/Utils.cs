8b19c228f07608ed33bec8eb7fee17bb5c1c3f78
C9Glax <13404778+C9Glax@users.noreply.github.com>
2022-11-18 15:54:55 +0100

    Changed Distance Calculation

--- Geo-Graph/Utils.cs
+++ Geo-Graph/Utils.cs
@@ -12,19 +12,40 @@
             return DistanceBetween(n1.lat, n1.lon, lat2, lon2);
         }
 
-        public static double DistanceBetween(float lat1, float lon1, float lat2, float lon2)
+        public static double DistanceBetween(float lat1, float lon1, Node n2)
         {
-            const int earthRadius = 6371000;
-            double differenceLat = DegreesToRadians(lat2 - lat1);
-            double differenceLon = DegreesToRadians(lon2 - lon1);
+            return DistanceBetween(n2, lat1, lon1);
+        }
 
-            double lat1Rads = DegreesToRadians(lat1);
-            double lat2Rads = DegreesToRadians(lat2);
+        public static double DistanceBetween(float lat1, float lon1, float lat2, float lon2) //Law of Cosines
+        {
+            /*
+             * From https://www.movable-type.co.uk/scripts/latlong.html
+             */
+            double rlat1 = DegreesToRadians(lat1);
+            double rlat2 = DegreesToRadians(lat2);
+            double drlon = DegreesToRadians(lon2 - lon1);
+            const int R = 6371;
 
-            double a = Math.Sin(differenceLat / 2) * Math.Sin(differenceLat / 2) + Math.Sin(differenceLon / 2) * Math.Sin(differenceLon / 2) * Math.Cos(lat1Rads) * Math.Cos(lat2Rads);
+            double d = Math.Acos(Math.Sin(rlat1) * Math.Sin(rlat2) + Math.Cos(rlat1) * Math.Cos(rlat2) * Math.Cos(drlon)) * R;
+
+            return d;
+        }
+
+        public static double DistanceBetweenHaversine(float lat1, float lon1, float lat2, float lon2)
+        {
+            /*
+             * From https://www.movable-type.co.uk/scripts/latlong.html
+             */
+            const int R = 6371;
+            double rlat1 = DegreesToRadians(lat1);
+            double rlat2 = DegreesToRadians(lat2);
+            double rdlat = DegreesToRadians(lat2 - lat1);
+            double drlon = DegreesToRadians(lon2 - lon1);
+            double a = Math.Sin(rdlat / 2) * Math.Sin(rdlat / 2) + Math.Cos(rlat1) * Math.Cos(rlat2) * Math.Sin(drlon / 2) * Math.Sin(drlon / 2);
             double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
-
-            return earthRadius * c;
+            double d = R * c;
+            return d;
         }
 
         private static double DegreesToRadians(double deg)
